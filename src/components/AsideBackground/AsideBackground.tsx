5d1d9979ecf6263872253b87f82a32a9a2acaab9
emanuelpps <emanuelpages.ps@gmail.com>
2024-11-05 14:46:14 -0300

    first commit

---
+++ src/components/AsideBackground/AsideBackground.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const AsideBackground: React.FC = () => {
+  return <div>AsideBackground</div>;
+};
---
+++ src/components/HeaderMobile/HeaderMobile.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const HeaderMobile: React.FC = () => {
+  return <div>HeaderMobile</div>;
+};
