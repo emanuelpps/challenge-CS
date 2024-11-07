2ec12e520348a98ee5ed7999d2b29ee54e466ff6
emanuelpps <emanuelpages.ps@gmail.com>
2024-11-06 22:58:20 -0300

    fix: added server corrections

--- src/Form/Form.tsx
+++ src/Form/Form.tsx
@@ -1,11 +1,18 @@
-import React from "react";
-import { FormContainer, StyledForm, FinishButton, CsLogoContainer, CsLogoImage } from "./Styles/Styles";
+import React, { useState } from "react";
+import {
+  FormContainer,
+  StyledForm,
+  FinishButton,
+  CsLogoContainer,
+  CsLogoImage,
+} from "./Styles/Styles";
 import { Outlet, useLocation } from "react-router-dom";
 import { ButtonFoward } from "../components/Buttons/ButtonFoward";
 import useFormProgress from "../Store/FormProgress";
 import CsLogo from "/assets/images/cs-logo.svg";
 
 export const Form: React.FC = () => {
+  const [response, setResponse] = useState(null);
   const { progress } = useFormProgress();
   const { pathname } = useLocation();
 
@@ -13,12 +20,19 @@ export const Form: React.FC = () => {
 
   const handleFinish = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    const response = await fetch("https://api.resend.com/api/email", {
-      method: "POST",
-    });
-    const data = await response.json()
-    console.log(data)
+
+    try {
+      const response = await fetch("http://localhost:3000/api/email", {
+        method: "POST",
+      });
+      const data = await response.json();
+      setResponse(data);
+    } catch (error: string) {
+      console.error(error);
+      setResponse(error);
+    }
   };
+
 
   return (
     <FormContainer>
@@ -29,7 +43,11 @@ export const Form: React.FC = () => {
         <Outlet />
         {pathname === "/step-6" && (
           <FinishButton>
-            <ButtonFoward text="Finalizar" type="button" onClick={handleFinish} />
+            <ButtonFoward
+              text="Finalizar"
+              type="button"
+              onClick={handleFinish}
+            />
           </FinishButton>
         )}
       </StyledForm>
--- src/api/emails/route.ts
+++ src/api/emails/route.ts
@@ -7,7 +7,7 @@ const resend = new Resend("re_M5LQ4Vic_JMv3msdni4qMQdYqX3azR5km")
 
 export async function POST(){
   await resend.emails.send({
-    from: 'onboarding@resend.dev',
+    from: 'emanuel737@gmail.com',
     to: 'emanuelpages.ps@gmail.com',
     subject: 'Cuestionario para ti',
     react: EmailTemplate(),
